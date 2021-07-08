305afc7465891bb24e7f751d1d591b493f08c5a2
Darwin Xu <darwin.xu@icloud.com>
2021-07-08 20:20:02 +0800

    add  a new function compare_string2()

--- sort_string.cpp
+++ sort_string.cpp
@@ -9,11 +9,11 @@
 
 using namespace std;
 
-void swap_element(string *p1, string *p2)
+void swap_element(string* p1, string* p2)
 {
     string x = *p1;
-    *p1 = *p2;
-    *p2 = x;
+    *p1      = *p2;
+    *p2      = x;
 }
 
 int compare_string(string str1, string str2, int num)
@@ -36,6 +36,25 @@ int compare_string(string str1, string str2, int num)
         {
             return compare_string(str1, str2, num + 1);
         }
+    }
+}
+
+/*
+ * Return: -1 if str1 < str2
+ *          0 if str1 = str2
+ *          1 if str1 > str2
+ */
+int compare_string2(const string& str1, const string& str2)
+{
+    int l = std::min(str1.length(), str2.length());
+    for (int i = 0; i < l; ++i)
+    {
+        if (str1[i] < str2[i])
+            return -1;
+        else if ((str1[i] == str2[i]))
+            return 0;
+        else
+            return 1;
     }
 }
 
@@ -68,19 +87,18 @@ void input_string(string list[], string str)
         carry[1] = str[i * 4 + 1];
         carry[2] = str[i * 4 + 2];
         carry[3] = str[i * 4 + 3];
-        list[i] = carry;
+        list[i]  = carry;
     }
 }
 
 void sort_string(string str[])
 {
-
     for (int i = 0; i < 5 - 1; i++)
     {
-        for (int j = i + 1; j < 5 - 1; j++)
+        for (int j = 0; j < 5 - i - 1; j++)
         {
-            if (compare_string(str[j], str[i], 0) == 1)
-                swap_element(&str[j], &str[i]);
+            if (compare_string(str[j], str[j + 1], 0) == 1)
+                swap_element(&str[j], &str[j + 1]);
         }
     }
 }
