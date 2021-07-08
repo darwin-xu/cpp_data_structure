09c524b77b016b22c00587f0b2d57994925be020
William <xu_qiran_william@163.com>
2021-07-08 19:28:09 +0800

    Some problems.

--- sort_string.cpp
+++ sort_string.cpp
@@ -9,6 +9,36 @@
 
 using namespace std;
 
+void swap_element(string *p1, string *p2)
+{
+    string x = *p1;
+    *p1 = *p2;
+    *p2 = x;
+}
+
+int compare_string(string str1, string str2, int num)
+{
+    if (num == 4)
+    {
+        return 0;
+    }
+    else
+    {
+        if ((char)str1[num] > (char)str2[num])
+        {
+            return 1;
+        }
+        else if ((char)str1[num] < (char)str2[num])
+        {
+            return 0;
+        }
+        else
+        {
+            return compare_string(str1, str2, num + 1);
+        }
+    }
+}
+
 void print_string(string str[], int strlen)
 {
     for (int i = 0; i < strlen; i++)
@@ -38,7 +68,7 @@ void input_string(string list[], string str)
         carry[1] = str[i * 4 + 1];
         carry[2] = str[i * 4 + 2];
         carry[3] = str[i * 4 + 3];
-        list[i]  = carry;
+        list[i] = carry;
     }
 }
 
@@ -52,6 +82,19 @@ void remove_element(string str[], int length, int num)
     str[length - 1] = "";
 }
 
+void sort_string(string str[])
+{
+
+    for (int i = 0; i < 5 - 1; i++)
+    {
+        for (int j = i + 1; j < 5 - 1; j++)
+        {
+            if (compare_string(str[j], str[i], 4) == 1)
+                swap_element(&str[j], &str[i]);
+        }
+    }
+}
+
 int main()
 {
     // int *p;
@@ -61,16 +104,7 @@ int main()
     input_string(list, str);
     cout << "ORIGIN: " << endl;
     print_string(list, 5);
-    cout << "MATCHED" << endl;
-    string a[5];
-    a[0] = "aaa";
-    a[1] = "bbb";
-    a[2] = "ccc";
-    // p = &a;
-    remove_element(a, 5, 0);
-    for (int i = 0; i < 3; i++)
-    {
-        cout << a[i] << endl;
-    }
+    cout << "MATCHED:" << endl;
+    print_string(list, 5);
     return 0;
 }
